2adf477462d3666af9f52daacfd598b7731ca4ff
Amine <amine.bensalah@intech-consulting.fr>
2022-01-16 09:53:22 +0100

    remove not needed files

--- lib/Universal/PhenixSdk.framework/Headers/PhenixDimensions.h
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright 2018 PhenixP2P Inc. Confidential and Proprietary. All Rights Reserved.
- */
-#include <stddef.h>
-
-struct PhenixDimensions {
-  size_t width;
-  size_t height;
-};
